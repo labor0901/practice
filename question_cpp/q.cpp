fa208c69a9c913e722b6935207a2a0d632af3a1b
labor0901 <131850930+labor0901@users.noreply.github.com>
2025-01-25 16:15:22 +0900

    1/25/2025

---
+++ question_cpp/q.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int i;
+    int a;
+    int r;
+    scanf("%d%d%d",&a,&r,&n);
+    for(i=1;n>i;i++)
+    {
+       long a = a*r;
+    }
+    printf("%d",a);
+}   
