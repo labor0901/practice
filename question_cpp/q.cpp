9c7f3b44ee027085fa21fb2fe371e3827f8d475e
labor0901 <131850930+labor0901@users.noreply.github.com>
2025-01-26 19:07:15 +0900

    1/26/2025 p3

--- question_cpp/q.cpp
+++ question_cpp/q.cpp
@@ -1,13 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int n;
-    int i;
-    long long int a,m,d;
-    scanf("%lld%lld%d",&a,&m,&d,&n);
-    for(i=1;n>i;i++)
-    {
-        a=a*m+d;
-    }
-    printf("%lld",a);
+    long long int a,b,c;
+    long long int d = 1;
+    scanf("%lld%lld%lld",&a,&b,&c);
+    while(d%a!=0 || d%b!=0 || d%c!=0)d++;
+    printf("%lld",d);
 }   
