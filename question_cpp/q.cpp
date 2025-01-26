64c75164dd661d476d257b0a04e85d87e13d5cd1
labor0901 <131850930+labor0901@users.noreply.github.com>
2025-01-26 19:33:12 +0900

    Update q.cpp

--- question_cpp/q.cpp
+++ question_cpp/q.cpp
@@ -1,16 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, t;
-    int a[24]={};
-    scanf("%d", &n); //개수 입력받기
-    for(i=1; i<=n; i++) //개수만큼 입력받기
+    int n, i, k;
+    int a[10001]={};
+    scanf("%d", &n); 
+    for(i=1; i<=n; i++) 
     {  
-        scanf("%d", &t); //읽어서
-        a[t]=a[t]+1; //들어있던 값에 1만큼 더해 다시 저장. a[t]+=1 과 같다.
+        scanf("%d", &a[i]); 
     }
-    for(i=1; i<=23; i++)
+    for(i=n; i>0; i--)
     {
-        printf("%d ", a[i]); //1~23 번 배열에 저장되어있는 값 출력하기
+        printf("%d ", a[i]); 
     }
 }
