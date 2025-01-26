c8c8f70fc0a25662f9b4522566cc00dc2e5ac72d
labor0901 <131850930+labor0901@users.noreply.github.com>
2025-01-26 20:21:19 +0900

    1/26/2025 p5

--- question_cpp/q.cpp
+++ question_cpp/q.cpp
@@ -1,15 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, k;
-    int a[10001]={};
-    scanf("%d", &n); 
-    for(i=1; i<=n; i++) 
-    {  
-        scanf("%d", &a[i]); 
-    }
-    for(i=n; i>0; i--)
+    int a[10000]={};
+    int n,i,k,s;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
-        printf("%d ", a[i]); 
+        scanf("%d",&a[i]);
     }
+    for(i=1;i<=n;i++)
+    {
+        k=1;
+        while(a[i]<=a[k]==true && k<=n)
+        {
+            if(k==10) s=a[i];
+            k++;
+        }
+    }
+    printf("%d",s);
 }
