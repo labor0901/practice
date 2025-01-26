efd7815c0c745b7f1df0fd65d81c48c186b2b65a
labor0901 <131850930+labor0901@users.noreply.github.com>
2025-01-26 18:46:40 +0900

    1/26/2025 p2

--- question_cpp/q.cpp
+++ question_cpp/q.cpp
@@ -3,12 +3,11 @@ int main()
 {
     int n;
     int i;
-    long long int a;
-    long int r;
-    scanf("%d%d%d",&a,&r,&n);
+    long long int a,m,d;
+    scanf("%lld%lld%d",&a,&m,&d,&n);
     for(i=1;n>i;i++)
     {
-        a=a*r;
+        a=a*m+d;
     }
     printf("%lld",a);
 }   
