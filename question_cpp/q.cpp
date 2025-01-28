6615113332a809d7c3929c64b5e7efd8603c60f2
labor0901 <131850930+labor0901@users.noreply.github.com>
2025-01-28 22:20:15 +0900

    Update q.cpp

--- question_cpp/q.cpp
+++ question_cpp/q.cpp
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int a[10000]={};
-    int n,i,k,s;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int n, i, j, x, y;
+    int a[20][20]={};
+    scanf("%d", &n);
+    for(i=1; i<=n; i++)
     {
-        scanf("%d",&a[i]);
+        
+        scanf("%d %d", &x, &y);
+        a[x][y]=1;
     }
-    for(i=1;i<=n;i++)
+    for(i=1; i<=19; i++) //한 줄(위에서 아래로) 씩
     {
-        k=1;
-        while(a[i]<=a[k]==true && k<=n)
-        {
-            if(k==10) s=a[i];
-            k++;
+        for(j=1; j<=19; j++) //한 열(왼쪽에서 오른쪽으로) 씩
+        {    
+            printf("%d ", a[i][j]); //값 출력
         }
+        printf("\n"); //줄 바꾸기
     }
-    printf("%d",s);
 }
